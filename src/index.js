ef06b876e7d30f025813753c5832e1297ab3fc58
kennesmit <74009296+kennesmit@users.noreply.github.com>
2020-11-17 15:36:05 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.render(
+    <Auth0Provider
+    domain="dev-jm44kztf.us.auth0.com"
+    clientId="2mSD2My8zLYTzyNcgn1CRuJULuMehQaJ"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+  
+);
